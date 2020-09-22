a0667a42b899ad03a0f31e47df7e1ee8ab3c1ac5
@Noch4nce <m4rgera123@mail.ru>
2020-09-22 20:58:33 +0500

    asd

---
+++ trening/arrayTren.js
@@ -0,0 +1,48 @@
+// let obj = {
+//     firstDay: 'Monday',
+//     secondDay: 'Thuesday',
+//     thirdDay: 'Wednesday',
+//     fourthDay: 'Thursday',
+//     fifthDay: 'Friday',
+//     sixthDay: 'Saturday',
+//     seventhDay: 'Sunday'
+// }
+// let day = obj.fourthDay;
+// console.log(day)
+// let array = [ [1, 2, 3], [4, 5, 6], [7,8,9] ]
+// console.log (array['a'][0])  
+
+// let obj = {
+//     js:['jQuery', 'Angular'], 
+//     php: 'hello', 
+//     css: 'world'
+// }
+// console.log(obj.js[0])
+
+// let twoArrays = {
+//     ru:['Понед', 'Вторник', 'Среда'], 
+//     en:['Monday', 'Thuesday', 'Wednesday']
+// }
+// console.log(twoArrays.ru[0])
+// console.log(twoArrays.en[2])
+
+    // let lang = {
+    //     ru: ['Понед', 'Вторник', 'Среда'],
+    //     en: ['Monday', 'Thuesday', 'Wednesday'],
+    //     day = lang.ru[2],
+    // }
+    // var arr = {
+    //     ru:['пон', 'втр', 'ср', 'чет', 'пят', 'сб', 'вс', ],
+    //     en:['mon', 'tue', 'wen', 'thur', 'fri', 'str', 'sun',]
+    //     };
+        
+    //     var lang = arr.ru;
+    //     var day = 5;
+        
+    //    console.log(arr['lang'],['day']);
+    
+    var lang = 'ru';
+    var day = 3;
+    alert(arr[lang],[day]);
+
+
